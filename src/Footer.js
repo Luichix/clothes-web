4bfff6f4fd9aea20cb64dcccb1fb59fe9b90a8f8
Luichix <luigui.rex@gmail.com>
2022-02-22 08:08:52 -0600

    actualizacion de diseño

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+function Footer () {
+    return(
+    <footer class="footer-container">
+		<h1 className='store-name'>VERO <i className='store-name-i'>Store</i></h1>
+		<div class="credits">
+			<p>Created © 2022</p>
+			<p>Diseñado por: <a href="https://luichix.github.io/portafolio/" target="_blank" rel="noreferrer"><code>Luichix Rex</code></a></p>
+        </div>
+	</footer>
+    );
+};
+
+export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header (){
     
     return (
     <header className="header-container">
-        <h1 className='store-name'>Vero <i className='store-name-i'>Store</i></h1>
+        <h1 className='store-name'>VERO <i className='store-name-i'>Store</i></h1>
         <nav className="nav-header">
             <a className="a-nav" href=".app">NUEVO</a>
             <a className="a-nav" href=".app">DESCUENTOS</a>
